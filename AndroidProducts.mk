dcda1bc7aee68c3448fcb146825e5aece4d1657c
Eric Schäfer <151476611+schaefereric@users.noreply.github.com>
2025-07-31 20:11:28 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_bqeelytwo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_bqeelytwo-userdebug
---
+++ lineage_bqeelytwo.mk
@@ -0,0 +1,9 @@
+
+
+$(call inherit-product, device/amlogic/gx-common/gx.mk)
+
+PRODUCT_NAME := lineage_bqeelytwo
+PRODUCT_DEVICE := bqeelytwo
+PRODUCT_BRAND := amlogic
+PRODUCT_MODEL := Bqeel_Y2
+PRODUCT_MANUFACTURER := amlogic
